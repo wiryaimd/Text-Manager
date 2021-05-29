9c4db2f0d90dec4166bbd29d7337720dc2b4e581
wiryaimd <adnyasutha003@gmail.com>
2021-05-29 18:18:16 +0800

    checking

--- app/src/main/java/com/wiryaimd/textmanager/MainActivity.java
+++ app/src/main/java/com/wiryaimd/textmanager/MainActivity.java
@@ -11,10 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        System.out.println("ecek aatu uwa iga najai guranjai awokawokawo");
-
-        int es = 0;
+        int iyah = 12;
 
     }
 }
