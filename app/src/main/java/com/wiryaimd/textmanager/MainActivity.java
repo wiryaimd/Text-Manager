48e74d50e76582cd37571cfe75078945b9017b5a
wiryaimd <adnyasutha003@gmail.com>
2021-05-30 09:52:27 +0800

    setup dependencies'

--- app/src/main/java/com/wiryaimd/textmanager/MainActivity.java
+++ app/src/main/java/com/wiryaimd/textmanager/MainActivity.java
@@ -2,18 +2,22 @@ package com.wiryaimd.textmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * MVVM architecture jrod
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int iyah = 12;
-
-        String iyahh = "hahay";
 
     }
 }
