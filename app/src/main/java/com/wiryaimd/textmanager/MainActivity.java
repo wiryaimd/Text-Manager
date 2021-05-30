9b9778f2e05327d5ea419fa40e7eae8cd5d0cdff
wiryaimd <adnyasutha003@gmail.com>
2021-05-30 10:44:49 +0800

    yeye

--- app/src/main/java/com/wiryaimd/textmanager/MainActivity.java
+++ app/src/main/java/com/wiryaimd/textmanager/MainActivity.java
@@ -28,6 +28,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        int cek = 1;
     }
 }
