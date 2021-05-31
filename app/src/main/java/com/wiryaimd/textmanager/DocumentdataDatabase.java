4eada5ec26c63fd8cd19027b7d138ad1aab61d7c
wiryaimd <adnyasutha003@gmail.com>
2021-05-31 11:49:01 +0800

    continue the code

--- app/src/main/java/com/wiryaimd/textmanager/DocumentdataDatabase.java
+++ app/src/main/java/com/wiryaimd/textmanager/DocumentdataDatabase.java
@@ -2,10 +2,13 @@ package com.wiryaimd.textmanager;
 
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 /**
  * membuat database ... maybe
@@ -27,10 +30,40 @@ public abstract class DocumentdataDatabase extends RoomDatabase {
             instance = Room
                     .databaseBuilder(context, DocumentdataDatabase.class, "textmanager_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(prepareData)
                     .build();
 
         }
         return instance;
+    }
+
+
+    /**
+     * generate data saat instance pertama kali dibuat
+     */
+
+    private static RoomDatabase.Callback prepareData = new Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PrepareDataAsync(instance).execute();
+        }
+    };
+
+    private static class PrepareDataAsync extends AsyncTask<Void, Void, Void>{
+
+        private DocumentdataDatabase db;
+
+        public PrepareDataAsync(DocumentdataDatabase db) {
+            this.db = db;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            db.documentdataDao().insert(new Documentdata("ngewe hayuu", "anjas guranjas takanjas anjas", "2021, 5, 31"));
+            db.documentdataDao().insert(new Documentdata("gile lu yak", "hahay papale", "2026, 4, 21"));
+            return null;
+        }
     }
 
 }
--- app/src/main/java/com/wiryaimd/textmanager/DocumentdataRepository.java
+++ app/src/main/java/com/wiryaimd/textmanager/DocumentdataRepository.java
@@ -1,10 +1,16 @@
 package com.wiryaimd.textmanager;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
 
 import java.util.List;
+
+/**
+ *  repository seperti / sebagai tempat manage data
+ *
+ */
 
 public class DocumentdataRepository {
 
@@ -16,5 +22,86 @@ public class DocumentdataRepository {
         documentdataDao = database.documentdataDao();
         allDocumentdata = documentdataDao.getDocumentdata();
     }
+
+    public void insert(Documentdata documentdata){
+        new InsertDoc(documentdataDao).execute(documentdata);
+    }
+
+    public void update(Documentdata documentdata){
+        new UpdateDoc(documentdataDao).execute(documentdata);
+    }
+
+    public void delete(Documentdata documentdata){
+        new DeleteDoc(documentdataDao).execute(documentdata);
+    }
+
+    public void deleteAll(){
+        new DeleteAllDoc(documentdataDao).execute();
+    }
+
+    public LiveData<List<Documentdata>> getAllDocumentdata() {
+        return allDocumentdata;
+    }
+
+    private static class InsertDoc extends AsyncTask<Documentdata, Void, Void>{
+
+        private DocumentdataDao dao;
+
+        public InsertDoc(DocumentdataDao documentdataDao){
+            this.dao = documentdataDao;
+        }
+
+        @Override
+        protected Void doInBackground(Documentdata... documentdata) {
+            dao.insert(documentdata[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateDoc extends AsyncTask<Documentdata, Void, Void>{
+
+        private DocumentdataDao dao;
+
+        public UpdateDoc(DocumentdataDao documentdataDao){
+            this.dao = documentdataDao;
+        }
+
+        @Override
+        protected Void doInBackground(Documentdata... documentdata) {
+            dao.update(documentdata[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteDoc extends AsyncTask<Documentdata, Void, Void>{
+
+        private DocumentdataDao dao;
+
+        public DeleteDoc(DocumentdataDao documentdataDao){
+            this.dao = documentdataDao;
+        }
+
+        @Override
+        protected Void doInBackground(Documentdata... documentdata) {
+            dao.delete(documentdata[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllDoc extends AsyncTask<Void, Void, Void>{
+
+        private DocumentdataDao dao;
+
+        public DeleteAllDoc(DocumentdataDao documentdataDao){
+            this.dao = documentdataDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            dao.deleteAllDocuments();
+            return null;
+        }
+    }
+
 
 }
