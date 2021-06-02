fc75b9eab04d678dc310d345561b633b51639eba
wiryaimd <adnyasutha003@gmail.com>
2021-06-02 12:24:44 +0800

    clean

--- app/src/test/java/com/wiryaimd/textmanager/ExampleUnitTest.java
+++ app/src/test/java/com/wiryaimd/textmanager/ExampleUnitTest.java
@@ -9,9 +9,13 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+
+public class ExampleUnitTest{
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void cekcrot(){
+
+        assertEquals(1, Thread.activeCount());
+
     }
 }
